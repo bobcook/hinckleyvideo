55e6697f3e969639f0921c77ed24e96a8f13649f
Bob Cook <bobcook@sunrem.com>
2015-05-12 16:50:29 -0600

    precompile assets for Heroku

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Hinckleyvideo
+Hinckleyvideo 
 ================
 
 [![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += [ 'minimalist.min.css', 'mediaelementplayer.min.css', 'jquery.fancybox.min.css', 'wp.css', 'style.min.css', 'default.min.css' ]
