6c8d0edcef22113a59c5eb558072473916ed09b8
enping <enpingan@gmail.com>
2015-05-15 21:24:37 +0800

    experian api

--- app/assets/javascripts/addition.js
+++ app/assets/javascripts/addition.js
@@ -1,3 +1,22 @@
+var Search_Config = {
+   AUTH_TOKEN: "23d9c794-5152-05d4-31fb-87936df8e6aa",
+   ADDRESS_SEARCH: true,
+   PHONE_SEARCH: true,
+   EMAIL_SEARCH: true,
+   ADDRESS_FIELDS: [
+      [{"name":"name","id":"name","mapping":["notMapped"]},{"name":"lastname","id":"lastname","mapping":["notMapped"]},{"name":"Address","id":"Address","mapping":["addressLine1"]},{"name":"City","id":"City","mapping":"city"},{"name":"State","id":"State","mapping":"state"},{"name":"Zip","id":"Zip","mapping":"zip"},{"name":"Country","id":"Country","mapping":"country"},{"name":"Phone","id":"Phone","mapping":"phone"},{"name":"email","id":"email","mapping":"email"}]
+   ],
+   
+   DATA_SETS: ["CAN","USA"]
+};
+
+(function (window, document) {
+   var url = "https://expdataqualitystatic.blob.core.windows.net/edqstatic/js-search-api-loader.js";
+   var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true; script.src = url;
+   var firstScript = document.getElementsByTagName("script")[0];
+   firstScript.parentNode.insertBefore(script, firstScript);
+})(window, document);
+
 
 $(document).ready(function(){
   $('#multi_sending').submit(function(){
@@ -33,7 +52,7 @@ $(document).ready(function(){
         // alert("sent ajax to url1");
         // alert(this.url);
         form = $('#multi_sending').serialize();  
-        
+
         $.ajax({
           type: 'POST',
           async: true,
@@ -44,7 +63,7 @@ $(document).ready(function(){
           // dataType: "jsonp",
           success: function(response) {
             // alert(this.url);
-            alert("sent ajax to url2");
+            // alert("sent ajax to url2");
           }
         });
       }
