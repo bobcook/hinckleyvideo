0b5f66488ed7ff2ee0bb9cb3a23283f0daeb2b1e
Bob Cook <bobcook@sunrem.com>
2015-05-19 14:32:45 -0600

    tryng to get fields to update

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,16 +1,18 @@
+require 'uri'
+
 class Lead < ActiveRecord::Base
   def self.send_data(req_info)
     new_record = Lead.new
 
-    new_record.first_name = req_info[:name]
-    new_record.last_name = req_info[:lastname]
-    new_record.address = req_info[:Address]
-    new_record.city = req_info[:City]
-    new_record.state = req_info[:State]
-    new_record.zip = req_info[:Zip]
-    new_record.country = req_info[:Country]
-    new_record.phone = req_info[:Phone]
-    new_record.email = req_info[:email]
+    new_record.first_name = URI.escape(req_info[:name])
+    new_record.last_name = URI.escape(req_info[:lastname])
+    new_record.address = URI.escape(req_info[:Address])
+    new_record.city = URI.escape(req_info[:City])
+    new_record.state = URI.escape(req_info[:State])
+    new_record.zip = URI.escape(req_info[:Zip])
+    new_record.country = URI.escape(req_info[:Country])
+    new_record.phone = URI.escape(req_info[:Phone])
+    new_record.email = URI.escape(req_info[:email])
 
     res = new_record.save
 
@@ -30,8 +32,12 @@ class Lead < ActiveRecord::Base
     send_url += '&F9list=Outbound%20Generated%20Leads&F9CallASAP=f9'
     send_url += '&first_name=' + new_record.first_name
     send_url += '&last_name=' + new_record.last_name
-    send_url += '&zip=' + new_record.zip
     send_url += '&street=' + new_record.address
+    send_url += '&city=' + new_record.city
+    send_url += '&state=' + new_record.state
+    send_url += '&zip=' + new_record.zip
+    send_url += '&Country=' + new_record.country
+    send_url += '&Email=' + new_record.email
 
     puts send_url
 
